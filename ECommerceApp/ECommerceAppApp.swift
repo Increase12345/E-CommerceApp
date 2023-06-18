2481075918031c5e49aa5b253d8ed4ce5d696212
Nick <pretki123@ukr.net>
2023-06-18 13:17:49 -0600

    Getting started

---
+++ ECommerceApp/ECommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECommerceAppApp.swift
+//  ECommerceApp
+//
+//  Created by Nick Pavlov on 6/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ECommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
